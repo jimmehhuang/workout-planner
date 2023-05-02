14364b7b09f3dbe618b51b5962961f398fcd92f0
Jimmy Huang <jimmymhuang@gmail.com>
2023-05-02 15:59:19 -0500

    fixed cors issue

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,12 @@ const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 const app =  express()
+const cors = require('cors')
 
 // middleware
 app.use(express.json())
+
+app.use(cors())
 
 app.use((req, res, next) => {
     console.log(req.path, req.method)
